34d632a26ebadb191aceed07334af32e94541f98
Yan Ferreira <yanfvc@gmail.com>
2024-09-15 19:38:10 -0300

    006 - Como receber validação de dados Request usando dd() e request->input

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,7 +13,9 @@ class AuthController extends Controller
 
     public function loginSubmit(Request $request)
     {
-        echo 'Login Submit';
+        echo $request->input(('text_username'));
+        echo '<br>';
+        echo $request->input(('text_password'));
     }
 
     public function logout()

5d3c9bb6483680cd8339b6e4b42fbb94855c79ba
Yan Ferreira <yanfvc@gmail.com>
2024-09-15 16:35:24 -0300

    002 - Criando View Main com Blade e recebendo parâmetro do Controller e exibindo com {{ }}.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +10,5 @@ Route::get('/', function () {
 Route::get('/about', function(){
     echo 'Sobre';
 });
+
+Route::get('/main/{value}', [MainController::class, 'index']);

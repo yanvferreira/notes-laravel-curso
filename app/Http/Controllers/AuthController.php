aa00c609ecdceb6cf4a3352d171c3db40ac4ea4a
Yan Ferreira <yanfvc@gmail.com>
2024-09-19 00:20:38 -0300

    010 - Conectando ao banco de dados postgres.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AuthController extends Controller
 {
@@ -33,7 +34,15 @@ class AuthController extends Controller
         $username = $request->input('text_username');
         $password = $request->input('text_password');
 
-        echo 'OK';
+        // test database connection
+        try{
+            DB::connection()->getPdo();
+            echo 'Connection success';
+        } catch(\PDOException $e){
+            echo 'Connection failed: ' . $e->getMessage();
+        }
+
+        echo 'FIM';
     }
 
     public function logout()

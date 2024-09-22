4393e6c3e5078b052fb9f8655761f42f4052702c
Yan Ferreira <yanfvc@gmail.com>
2024-09-22 16:21:12 -0300

    016 - Criando middleware para bloquer acesso de usuario logado a pagina de login

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,16 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
 use App\Http\Middleware\CheckIsLogged;
+use App\Http\Middleware\CheckIsNotLogged;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login', [AuthController::class, 'login']);
-Route::post('loginsubmit', [AuthController::class, 'loginSubmit']);
+// Rotas acessíveis somente quando o usuario nao estiver logado
+Route::middleware([CheckIsNotLogged::class])->group(function() {
+    Route::get('/login', [AuthController::class, 'login']);
+    Route::post('loginsubmit', [AuthController::class, 'loginSubmit']);
+});
 
+// Rotas acessíveis somente quando o usuario estiver logado
 Route::middleware([CheckIsLogged::class])->group(function(){
     Route::get('/', [MainController::class, 'index']);
     Route::get('/newnote', [MainController::class, 'newNote']);
